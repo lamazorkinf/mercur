f1c5c2a7be4b5534f5345e8380a1203bc3238284
lamazorkinf <105391141+lamazorkinf@users.noreply.github.com>
2025-10-19 00:22:37 -0300

    Fix syntax errors in medusa-config.ts

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -1,11 +1,8 @@
-import { defineConfig, loadEnv } from '@medusajs/framework/utils'
-
+medusa config.ts: import { defineConfig, loadEnv } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
-
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL
-    },
+    databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -83,6 +80,9 @@ module.exports = defineConfig({
           }
         ]
       }
+    }
+  ]
+})
     }
   ]
 })
