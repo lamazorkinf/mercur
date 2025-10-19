e9798dce8b26b9aebff97639a19e6ef27029946f
lamazorkinf <105391141+lamazorkinf@users.noreply.github.com>
2025-10-19 00:21:39 -0300

    Update medusa-config.ts

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -4,9 +4,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    database_extra: {
-      ssl: false
+    databaseUrl: process.env.DATABASE_URL
     },
     http: {
       storeCors: process.env.STORE_CORS!,
